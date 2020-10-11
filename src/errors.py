d2e7df77f40234c0c86181af877db15cf0a00005
Stoyan Vitanov <stoyan.a.vitanov@gmail.com>
2020-10-12 00:28:53 +0300

    feat: add custom errors module

---
+++ src/errors.py
@@ -0,0 +1,9 @@
+"""
+This module defines common application error classes.
+"""
+
+class InvalidInputException(Exception):
+    """
+    InvalidInputException defines an exception raised
+    after invalid input.
+    """
